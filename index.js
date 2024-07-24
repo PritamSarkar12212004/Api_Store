11f529cce7fe400894529082ef67f02d2260d92a
pritam sarkar <pritam7796419792@gmail.com>
2024-07-24 15:57:27 +0530

    env set

--- index.js
+++ index.js
@@ -2,19 +2,17 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const User = require("./models/Models");
+require("dotenv").config();
 const app = express();
 
 app.get("/", async function (req, res) {
   let data = await User.find({});
   res.send(data);
 });
-
 // data server code for mongo db
 async function connectToDatabase() {
   try {
-    await mongoose.connect(
-      "mongodb+srv://pritam7796419792:Api%401234@api.gnnrght.mongodb.net/?retryWrites=true&w=majority&appName=Api"
-    );
+    await mongoose.connect(process.env.MONGO_URL);
     console.log("Database is connected");
   } catch (error) {
     console.error("Database connection error:", error);
